826f88c143c02c07e73420b3a08b60f4b99c15dc
Bizi Kneidi <tobias.kneidinger@gmail.com>
2019-02-08 12:53:53 +0100

    Added reactivex-example

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // require("./examples/callbacks")
 // require("./examples/promises")
-require("./examples/async-await")
+// require("./examples/async-await")
+const Rx = require("rxjs/Rx")
+
+Rx.Observable.from([1,2,3]).subscribe((item) => console.log(item));
